76ee47dbf06e90bb983f947197298bd5349a3135
Geunsik Lim <geunsik.lim@samsung.com>
2018-08-22 19:48:56 +0900

    Updated default waiting time to get a normal request from server
    This commit is to update a default waiting time to get a normal request from server.
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- run-all-issues.sh
+++ run-all-issues.sh
@@ -10,6 +10,8 @@
 
 # ----------------------------- Do modify the below statements ----------------------
 # Please modify the variable ISSUE_LAST by running "./gh-issue-import-ng.py --all" command.
+echo -e "\n\n\n"
+echo -e "Starting a github issue mover..."
 echo -e "[DEBUG] Please modify the variable ISSUE_LAST by running './gh-issue-import-ng.py --all' command."
 # You can get the "399 new issues" message.
 declare -i ISSUE_START
@@ -20,11 +22,12 @@ ISSUE_LAST=154
 # We have to wait for 3 minutes on average whenever 30 issues are proceeded to avoid service denial.
 # If you do not use appriate items and wait time, you will meet " There was a problem during authentication." message.
 # We have set a heuristic values from our experience to do not meet unexpected situation such as service error.
-# We recommend that you try to set ISSUE_ITEMS among 20, 30 (recommended), 40, 50, and 60.
-ISSUE_ITEMS=30
-ISSUE_WAIT_TIME=$((60*20))
+# We recommend that you try to set ISSUE_ITEMS among 20(recommended), 30, 40, 50, and 60.
+ISSUE_ITEMS=20
+ISSUE_WAIT_TIME=$((60*15))
 
 # ----------------------------- Do not modify the below statements ----------------------
+echo -e "Error Report:" > ./output.log
 for ((i=$ISSUE_START; i<=$ISSUE_LAST;i++)); do
     echo -e "################ Transferring issue '$i' #########################"
     # If you do not append --ignore-milestone --ignore-labels, you will meet "DETAILS: Validation Failed" message.
