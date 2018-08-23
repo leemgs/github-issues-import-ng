0dc433270373b6d3d11059957a55697038329782
Geunsik Lim <geunsik.lim@samsung.com>
2018-08-23 13:44:53 +0900

    Avoid service denial due to rate limit with heuristic time handling
    This commit is to append import_err_handling_level policy with a heuristic
    time handling to control a service denial situation due to rate limit policy
    of github.com
    
    * import_err_handling_level=0 (Recommended)
    * import_err_handling_level=1
    
    In case of that the import operation generates an error situation,
    0 means that the program just stops the program after display a reason of the error.
    1 means that the program try to import issue again after waiting for appropriate time.
    
    **Changes proposed in this PR:**
    
    * Version 1:
    1. Added error handling level
    2. Added wating time level
    3. Appended execption handling
    
    * Version 2:
    1. Added usage to handle arguments
    2. Separated wait time to static time and dynamic time
    
    * Version 3:
    1. Improved a time calculation method.
    2. Added color statement for debugging and readability
    
    * Version 4:
    1. Updated main document (README.md)
    2. Updated config.init.sample for users
    3. Added screenshot file.
    
    Signed-off-by: Geunsik Lim <leemgs@gmail.com>

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 
-### GitHub Issues Import - NG ###
+## Introduction ##
+The **github-issue-import-ng** repository is to import issues and pull requests from one repository to another; works even for private repositories, and if the two repositories are not related to each other in any way.
 
-This Python script allows you to import issues and pull requests from one repository to another; works even for private repositories, and if the two repositories are not related to each other in any way.
-
-The **github-issues-import-ng** repository is forked from the below websites to transfer issues and pull requests between github and github (or github enterprise) repository.
+It is forked as a next generation github issue importer from the below websites to transfer issues and pull requests between github and github (or github enterprise) repository.
 * Original website - [`mokorenkov/tools`](https://github.com/mkorenkov/tools).
 * Forked website - [`IQAndreas/github-issues-import`](https://github.com/IQAndreas/github-issues-import).
 
-It is forked as a next-generation github issue importer. The goal of this repository is as following:
+The goal of this repository is as following:
 * Support a stability by fixing bugs that people report
 * Keep a maintenance to review and merge pull requests
 * Add a new features that are proposed via a pull request process by contributors
 * Support a issue migration between github enterprise and github community as well as github communities
 
 
-### Getting Started ###
-First of all, you have to create a config.ini file to apply your github repositories. Then, run ./run-all-issues.sh command.
+## Getting Started ##
+To quickly get started, rename `config.ini.sample` to `config.ini`, and edit the fields to match your login info and repository info. If you want to store `config.ini` file in a different folder, use the `--config <file>` option to specify which config file to load in.
 ```bash
 $ cp ./config.ini.sample ./config.ini
 $ vi ./config.ini
@@ -24,54 +23,39 @@ $ ./run-all-issue.sh
 ```
 
 
-#### Usage ####
+## Advanced ##
 
-The script will by default look for a file named `config.ini` located in the same folder as the Python script. For a list of all possible configuration options, see [_Configuration_](http://www.iqandreas.com/github-issues-import/configuration/).
-
-To quickly get started, rename `config.ini.sample` to `config.ini`, and edit the fields to match your login info and repository info. If you want to use a different credentials for the source and target repositories, please see [_Configuration: Enterprise Accounts and Advanced Login Options_](http://www.iqandreas.com/github-issues-import/configuration/#enterprise). Store the config file in the same folder as the `gh-issues-import.py` script, or store it in a different folder, using the `--config <file>` option to specify which config file to load in.
-
-**Warning:** The password is stored in plain-text, so avoid storing the config file in a public repository. To avoid this, you can instead pass the username and/or password as arguments by using the `-u <username>` and `-p <password>` flags respectively. If the username or password is not passed in from either of these locations, the user will be prompted for them when the script runs.
- 
 Run the script with the following command to import all open issues into the repository defined in the config:
 
-```
- $ python3 gh-issues-import.py --open
+```bash
+ $ python3 gh-issues-import-ng.py --open
 ```
 
-If you want to import all issues (including the closed ones), use `--all` instead of `--open`. Closed issues will still be open in the target repository, but titles will begin with `[CLOSED]`.
+If you want to import all issues including the closed ones, use `--all` instead of `--open`. Closed issues will be automatically closed in the target repository, but titles will begin with `[ISSUE][CLOSED]` and `[PR][CLOSE]`.
 
-Or to only import specific issues, run the script and include the issue numbers of all issues you wish to import (can be done for one or several issues, and will even include closed issues):
+Or to only import specific issues, run the script and include the issue numbers of all issues you wish to import.
 
 ```
  $ python3 gh-issues-import.py --issues 25 26 29
 ```
 
-Some config options can be passed as arguments. For a full list, see [the the _Arguments_ page](http://www.iqandreas.com/github-issues-import/arguments/), or run the script using the `--help` flag.
-
-#### Result ####
-
-Every issue imported will create a new issue in the target repository. Remember that the ID of the issue in the new repository will most likely not be the same as the ID of the original issue. Keep this in mind when writing commit messages such as _"Closes #25"_.
+Every issue imported will create a new issue in the target repository. Remember that the ID of the issue in the new repository will most likely not be the same as the ID of the original issue. 
 
 If the issue is a pull request, this will be indicated on the issue, and a link to the code will be provided. However, it will be treated as a new issue in the target repository, and **not** a pull request. Pulling in the suggested code into the repository will need to be done manually.
 
-Any comments on the issue will be imported, however, the author of all imported comments will be the account specified in the config. Instead, a link and header is provided for each comment indicating who the original author was and the original date and time of the comment. Any subsequent comments added to the issue after it has been imported into the target repository will not be included.
+Any comments on the issue will be imported, however, the author of all imported comments will be the account specified in the `config.ini` file. Instead, a link and header is provided for each comment indicating who the original author was and the original date and time of the comment. Any subsequent comments added to the issue after it has been imported into the target repository will not be included.
 
 Labels and milestones attached to the issue will be imported and added to the target repository if they do not already exist there. If the label or milestone with the same name already exists, the issue will point to the existing one, and any difference in the description or other details will be ignored.
 
-If allowed by GitHub's policies, it may be a good idea to use a "neutral" account to import the issues and issue comments to avoid imported comments from getting mixed up with developer comments (example: [FlixelCommunityBot](https://github.com/FlixelCommunityBot?tab=activity)).
+If allowed by GitHub's policies, it may be a good idea to use a token key instead of a password of a "neutral" account to import the issues and issue comments.
+* https://developer.github.com/apps/building-github-apps/understanding-rate-limits-for-github-apps/
+* https://developer.github.com/v3/rate_limit/
 
-#### Templates ####
-
-The script will by default use the [Markdown-formatted](http://github.github.com/github-flavored-markdown/) templates found in the [`templates`]({{site.github_url}}/tree/master/templates/) directory. You can edit those, or point to your own templates from the config file; see [_Custom Templates_](http://www.iqandreas.com/github-issues-import/templates/) for more details.
-
-#### Examples ####
-
-[![Example result of an imported pull request](http://www.iqandreas.com/github-issues-import/example-imported-issue.png)](https://github.com/IQAndreas-testprojects/github-issues-import-example/issues/8)
+## Evaluation ##
+<img src=./screenshot01.png>
 
 * [**Example issue (with label)**](https://github.com/IQAndreas-testprojects/github-issues-import-example/issues/8) ([original](https://github.com/IQAndreas/github-issues-import/issues/1))
 * [**Example pull request**](https://github.com/IQAndreas-testprojects/github-issues-import-example/issues/9) ([original](https://github.com/IQAndreas/github-issues-import/issues/2))
 * [**Example issue with comments**](https://github.com/IQAndreas-testprojects/github-issues-import-example/issues/10) ([original](https://github.com/IQAndreas/github-issues-import/issues/3))
 * [**Example issue with milestone**](https://github.com/IQAndreas-testprojects/github-issues-import-example/issues/11) ([original](https://github.com/IQAndreas/github-issues-import/issues/9))
-
-
 
--- run-all-issues.sh
+++ run-all-issues.sh
@@ -9,55 +9,154 @@
 # @author: Geunsik Lim <leemgs@gmail.com>
 
 # ----------------------------- Do modify the below statements ----------------------
-# Please modify the variable ISSUE_LAST by running "./gh-issue-import-ng.py --all" command.
-echo -e "\n\n\n"
-echo -e "Starting a github issue mover..."
-echo -e "[DEBUG] Please modify 'ISSUE_LAST' value by running './gh-issue-import-ng.py --all'."
-echo -e "[DEBUG] You can get the ISSUE_LAST value from 'output: xxx new issues' message."
-declare -i ISSUE_START
-declare -i ISSUE_LAST
-ISSUE_START=1
-ISSUE_LAST=154
+# Write your account name of github.com website
+github_com_id="leemgs"
+
+# Declare the number of issues that you want to maintain with a group to avoid a service denial.
+# Note: If you meet "ERROR: There was a problem during authentication", we recommend
+# that you try to define a smaller number instead of 20 (binding issues as a group) to avoid
+# this issue. For example, 20(default), 10, 8, 6, 4, and 2 issues.
+# When the number of specified issues are proceeded, the program sleep for a specified time.
+ISSUE_ITEMS_GROUP=2
 
 # ----------------------------- Do not modify the below statements ----------------------
+if [[ $1 == "--query" ]]; then
+    ./gh-issues-import-ng.py --all
+    exit 1
+elif [[ $1 == "" || $2 == "" ]]; then
+    echo -e "[DEBUG] Please run $0 correctly after reading the below usage."
+    echo -e "[DEBUG] If you want to know the last number of issues, visit source repository"
+    echo -e "[DEBUG] Usage: $0 [issue_start_no] [ issue_last_no]"
+    echo -e "[DEBUG] Usage: $0 --query"
+    exit 1
+else
+    echo -e ""
+fi
+ISSUE_START=$1
+ISSUE_LAST=$2
+
+#Color setting
+NC='\033[0m'       # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Display initial messages
+echo -e "\n\n\n"
+echo -e "${Red}Starting a github issue mover...${NC}"
+echo -e "[DEBUG] Please modify 'ISSUE_LAST' value by running './gh-issue-import-ng.py --all'."
+echo -e "[DEBUG] You can get the 'ISSUE_LAST' value from 'output: xxx new issues' message."
+
 # Understanding the rate limit rule of github.com
 # First of all, we have to find out the maximum number of requests we are permitted
 # by the rate limit rules of github.com by running as following:
 # 1. anonymous access: "curl -i https://api.github.com/users/<user_id> | grep RateLimit"
 # 2. token key access: "curl -i https://api.github.com/users -u <user_id>:<token_key> | grep RateLimit"
-#
-# If you don't know an appriate items and a wait time, you will meet the below error message.
-# "There was a problem during authentication." message.
-# So, we have set a heuristic value from our experience to do not meet unexpected situation
-# such as service error. We recommend that you try to set ISSUE_ITEMS among 20(recommended),
-# 30, 40, 50, and 60. We have to wait for 20 minutes on average whenever 20 issues are
-# proceeded to avoid service denial.
-ISSUE_ITEMS=20
-ISSUE_WAIT_TIME=$((60*20))
+
+
+# Declare wait time that you want to sleep in case of a service denial from github.com.
+# We have to wait for 5 minutes by default on average whenever 20 issues are proceeded.
+# Note: If you meet "ERROR: There was a problem during authentication", we recommend
+# that you try to define a larger number instead of 5 (minutes) to avoid this issue.
+# For example, 5 (default), 30, 60, 120, 180, 240, and 300 minutes.
+USER_DEFINED_WAIT_TIME=5
+
+# The variable WAIT_TIME_LEVEL has two levels.
+# By default, 0 is static time. 1 is dynamic time.
+WAIT_TIME_LEVEL=0
+
+
 LOG_FILE="output.log"
 
 # If you meet suddenly a "DETAILS: Validation Failed" message, we recommend that you try to
 # append "--ignore-milestone --ignore-labels",
-IMPORT_OPTION="--ignore-milestone --ignore-labels --ignore-pull-requests"
+# IMPORT_OPTION="--ignore-milestone --ignore-labels --ignore-pull-requests"
+IMPORT_OPTION="--ignore-milestone --ignore-labels"
+
+# in case of that the import operation generates error situation,
+# 0 (for newbie) means that the program just stops the program after display a reason of the error.
+# 1 (for expert) means that the program try to import issue again after waiting for appropriate time.
+IMPORT_ERR_HANDLING_LEVEL=0
 
 echo -e "Error Report:" > $LOG_FILE
 for ((i=$ISSUE_START; i<=$ISSUE_LAST;i++)); do
-    echo -e "################ Transferring issue '$i' #########################"
+    echo -e "${Green}####################### Transferring issue '$i' #############################${NC}"
+    echo -e "[DEBUG] Run 'yes Y | ./gh-issues-import-ng.py -i $i $IMPORT_OPTION'"
     yes Y | ./gh-issues-import-ng.py -i $i $IMPORT_OPTION
-    if [[ $? == 0 ]]; then
+    result=$?
+    if [[ $result == 0 ]]; then
         echo -e "Successfully transferred issue $i."
     else
         # Save error messages into the log file.
-        echo -e "Oooops. Issue $i is failed. The message is saved in $LOG_FILE file."
+        echo -e "${Red}Oooops. Issue $i is failed. The message is saved in $LOG_FILE file.${NC}"
         echo -e "Issue $i is failed." >> $LOG_FILE
-        # Let's stop the program.
-        exit 1
+        # Save current limit information
+        curl -i https://api.github.com/users/$github_com_id | grep RateLimit >> $LOG_FILE
+        epoch_limit_reset=`cat ./$LOG_FILE | grep ^X-RateLimit-Reset | cut -d ':' -f 2 | tr -d '[:space:]'`
+        echo -e "The rate limit (60 items) will be recharged after ${Blue}`date --date @${epoch_limit_reset}` (${epoch_limit_reset})${NC}."
+        # Stop the program in case of that IMPORT_ERR_HANDLING_LEVEL is 0.
+        if [[ $IMPORT_ERR_HANDLING_LEVEL == 0 ]]; then
+            echo -e "Stopping the program due to the service denial situation of github.com"
+            echo -e "${Red}Rerun $0 after modifying the variable ISSUE_START with $i.${NC}"
+            echo -e "${Red}For example,  $0 $i $2 ${NC}"
+            exit 1
+        # Retry an import operation in case that IMPORT_ERR_HANDLING_LEVEL is 1.
+        elif [[ $IMPORT_ERR_HANDLING_LEVEL == 1 ]]; then
+            while true; do
+                epoch_current_time=`date +%s`
+                if [[ $epoch_current_time -gt $epoch_limit_reset ]]; then
+                    echo -e "[DEBUG] Retry 'yes Y | ./gh-issues-import-ng.py -i $i $IMPORT_OPTION'"
+                    yes Y | ./gh-issues-import-ng.py -i $i $IMPORT_OPTION
+                    result_retry=$?
+                    if [[ $result_retry == 0 ]]; then
+                        echo -e "${Red}Rerunning is okay. Successfully transferred issue $i.${NC}"
+                        break
+                    else
+                        echo -e "Retrial is failed. Stopping the program..."
+                        echo -e "${Red}Rerun $0 after modifying the variable ISSUE_START with $i.${NC}"
+                        echo -e "${Red}For example,  $0 $i $2 ${NC}"
+                        exit 1
+                    fi
+                else
+                    echo -e "[$epoch_current_time] Sleeping for 1 minutes to keep limit rules of github..."
+                    sleep 60
+                fi
+            done
+        # TODO: if 2nd tryial is still failed,
+        # we need to increate a sleep time more (e.g., 2, 3, 4, and 5 hours).
+        else
+            echo -e "${Red}The variable IMPORT_ERR_HANDLING_LEVEL is unknown value.${NC}"
+        fi
     fi
 
-    # In case that an issue number is $ISSUE_ITEMS, ...
-    if [[ $(($i % $ISSUE_ITEMS)) == 0 ]]; then
-        echo -e "Freezing this task for $ISSUE_WAIT_TIME seconds to avoid a service denial..."
-        sleep $ISSUE_WAIT_TIME
+    # If an issue number is equal to $ISSUE_ITEMS_GROUP, sleep for a specified time.
+    if [[ $(($i % $ISSUE_ITEMS_GROUP)) == 0 ]]; then
+
+        if [[ $WAIT_TIME_LEVEL == 0 ]]; then
+            # Wait for static time
+            ISSUE_WAIT_TIME=$((60*$USER_DEFINED_WAIT_TIME))
+            echo -e "Freezing the task for $ISSUE_WAIT_TIME seconds (static time)..."
+            sleep $ISSUE_WAIT_TIME
+        elif [[ $WAIT_TIME_LEVEL == 1 ]]; then
+            # Wait for dynamic time after randomizing a waiting time between 5 and 9 minutes
+            while true; do
+                data=$((${RANDOM}%($USER_DEFINED_WAIT_TIME*2)))
+                if [[ $data -ge $USER_DEFINED_WAIT_TIME ]]; then
+                    echo -e "[DEBUG] randomized time (minutes) is $data."
+                    break
+                fi
+            done
+            ISSUE_WAIT_TIME=$((60*$data))
+            echo -e "Freezing the task for $ISSUE_WAIT_TIME seconds (dynamic time)..."
+            sleep $ISSUE_WAIT_TIME
+        else
+            echo -e "${Red}The variable WAIT_TIME_LEVEL is unknown value.${NC}"
+        fi
     else
         echo -e "Waiting for 1 seconds..."
         sleep 1
